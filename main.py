8b6edbd13dcc56c7555c8b47e3892d7375eee386
Никишев Николай Сергеевич <n.nikishev@comita.digital>
2025-08-07 23:09:34 +0700

    Revert "Revert "initial""
    This reverts commit 63f9bf9d7908108b3a614f1bb173417661b4f153.

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+)
+
+from app import views
+
+from settings import CORS_ORIGIN_WHITELIST
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+        swagger_favicon_url="/static/favicon.png",
+    )
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
+
+
+app.include_router(views.router, prefix="/api")
